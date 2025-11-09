97387f05b1d1e596a241502711f7039560cc0fbf
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2025-11-09 16:47:52 +0900

    マウスY軸回転を入れてみたの巻

--- camera.cpp
+++ camera.cpp
@@ -58,7 +58,7 @@ void InitCamera(void)
 	g_camera.posR = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	g_camera.vecU = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
 	g_camera.fDistanceFromVToR = 200.0f;
-	g_camera.rot.y = 0;
+	g_camera.rot = D3DXVECTOR3_ZERO;
 
 	g_camera.posR.x = g_camera.posV.x + sinf(g_camera.rot.y) * g_camera.fDistanceFromVToR;
 	g_camera.posR.z = g_camera.posV.z + cosf(g_camera.rot.y) * g_camera.fDistanceFromVToR;
@@ -194,6 +194,23 @@ void UpdateCamera(void)
 		g_camera.fDistanceFromVToR += 2.0f;
 		g_camera.posV.x = g_camera.posR.x + sinf(g_camera.rot.y + D3DX_PI) * g_camera.fDistanceFromVToR;
 		g_camera.posV.z = g_camera.posR.z + cosf(g_camera.rot.y + D3DX_PI) * g_camera.fDistanceFromVToR;
+	}
+
+	if (GetMousePress(MOUSE_LEFT))
+	{
+		DIMOUSESTATE mouse = GetMouse();
+
+		g_camera.rot.y += mouse.lX * 0.01f;
+		g_camera.posV.x = g_camera.posR.x + sinf(g_camera.rot.y + D3DX_PI) * g_camera.fDistanceFromVToR;
+		g_camera.posV.z = g_camera.posR.z + cosf(g_camera.rot.y + D3DX_PI) * g_camera.fDistanceFromVToR;
+	}
+	if (GetMousePress(MOUSE_RIGHT))
+	{
+		DIMOUSESTATE mouse = GetMouse();
+
+		g_camera.rot.y += mouse.lX * 0.01f;
+		g_camera.posR.x = g_camera.posV.x + sinf(g_camera.rot.y) * g_camera.fDistanceFromVToR;
+		g_camera.posR.z = g_camera.posV.z + cosf(g_camera.rot.y) * g_camera.fDistanceFromVToR;
 	}
 
 	if (GetKeyboardPress(DIK_RETURN))
